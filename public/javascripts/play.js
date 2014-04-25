2527b4bfdfd1ec703f3186fb78540614f744e215
Yuan Phoon <yuanphoon@gmail.com>
2014-04-26 00:16:24 +0100

    more comments
    Helps me to keep track of what's going on.

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -1,9 +1,14 @@
+// Variables
+
+// Set the variable of the target
 var targetWord = $('.target').text();
 console.log(targetWord);
 
+// Set the variable of the answer
 var answerWord = $('.answer').val();
 console.log(answerWord);
 
+// The score
 var score = 0;
 
 // When a key in the answer box is pressed, run function.
@@ -16,14 +21,17 @@ function updateVals() {
   console.log(targetWord);
   console.log(answerWord);
   
+  // If the answer matches the target, run the code within
   if (answerWord === targetWord) {
+    // Increase the score by 1
     score++;
-
+    // Clear the answer box
     $('.answer').val("");
 
   }
   console.log(score);
   
+  // Write the score into the scoreboard
   $('.score-number').text(score);
 }
 
