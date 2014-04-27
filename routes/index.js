f0aa791ea141c91f1e3417da406208050324aade
Yuan Phoon <yuanphoon@gmail.com>
2014-04-27 16:49:58 +0100

    text pruned and weight smaller

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Typing King' });
+  res.render('index', { title: 'Typing Mania' });
 });
 
 module.exports = router;
